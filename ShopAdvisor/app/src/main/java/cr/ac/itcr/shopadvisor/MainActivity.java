cb475298eec60a9c94fc528f85e18e777618e52a
Daryn Soto Soto <Daryn Soto Soto>
2016-03-02 13:30:26 -0600

    Segundo Commit

--- ShopAdvisor/app/src/main/java/cr/ac/itcr/shopadvisor/MainActivity.java
+++ ShopAdvisor/app/src/main/java/cr/ac/itcr/shopadvisor/MainActivity.java
@@ -1,10 +1,12 @@
 package cr.ac.itcr.shopadvisor;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,7 +14,44 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    protected void onStop() {
+        Log.d("Method", "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("Method", "onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("Method", "onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("Method", "onStop");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("Method", "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d("Method", "onRestart");
+        super.onRestart();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("Method", "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -24,6 +63,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Intent i = new Intent(MainActivity.this,DashboardActivity.class);
+                i.putExtra("messaje","DARYN SOTO SOTO");
+                startActivity(i);
             }
         });
     }
